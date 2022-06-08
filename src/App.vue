678414c56f35fb2c64036186e998872778c1414e
Giovanni Ciotta <giociotta@gmail.com>
2022-06-08 17:15:22 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <MyHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/myheader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHeader
   }
 }
 </script>
---
+++ src/components/Myheader.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>questo è l'header </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: ' MyHeader',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+  color: blue;
+}
+
+</style>
