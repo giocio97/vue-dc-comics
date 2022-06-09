445bf16aa5b86d81faa8456c4454ae4ebec27e5f
Giovanni Ciotta <giociotta@gmail.com>
2022-06-09 18:04:04 +0200

    prova di oggetto

---
+++ src/components/serie-dc.vue
@@ -0,0 +1,24 @@
+<templete>
+
+ <div class="card-serie">
+            <img :src="details.thumb" :alt="details.series">
+            <h4>{{details.series}}</h4>
+    </div>
+
+</templete>
+
+<script>
+export default {
+  name: 'SerieDc',
+  props:{
+      details:Object
+  }
+
+}
+</script>
+
+ <style scoped lang="scss">
+ 
+ 
+ 
+ </style>
